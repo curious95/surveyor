b9fd816b19ba468c08de30235c8419d9368df4ea
kamal <kamalpradhan95@outlook.com>
2020-08-05 23:58:26 +0000

    Refactored directory structure

---
+++ app/__main__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for, Response
+from sqlalchemy import exc
+from jinja2 import Template
+from datetime import datetime
+
+from app.models.models import Customers, History, Reminders
+from app.models.base_ import Base, engine, Session
+from app.configuration import Config
+
+import logging
+import traceback
+import glob
+
+app = Flask(__name__)
+config = Config()
+
+# logging.basicConfig(filename=f"/home/groot/workspace/customer_manager/app/logs/{datetime.today().strftime('%Y-%m-%d')}.log",
+#                     level=logging.DEBUG,
+#                     format=' %(asctime)s %(levelname)s %(name)s %(threadName)s: %(message)s')
+
+
+@app.route('/')
+def main():
+    return render_template('login.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
